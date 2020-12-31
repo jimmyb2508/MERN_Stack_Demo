3a17575c2dfb51fb16e8a2d495c1f9a24e207827
James Barrington <james.barrington1985@gmail.com>
2020-12-31 21:22:31 +0000

    First Commit

---
+++ server.js
@@ -0,0 +1,34 @@
+// Import npm packages
+const express = require('express');
+const mongoose = require('mongoose');
+const morgan = require('morgan');
+const path = require('path');
+
+const app = express();
+const PORT = process.env.PORT || 8080;
+
+const routes = require('./routes/api');
+
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/mern_youtube', {
+  useNewUrlParser:true,
+  useUnifiedTopology:true,
+});
+
+mongoose.connection.on('connected', () => {
+  console.log('Mongoose in connected!')
+});
+
+// Data Parsing
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+ 
+// HTTP request logger
+app.use(morgan('tiny'));
+app.use('/api', routes);
+
+if(process.env.NODE_ENV === 'production') {
+  app.use(express.static('client/build'));
+}
+
+app.listen(PORT, console.log(`Server is starting on ${PORT}`));
+
